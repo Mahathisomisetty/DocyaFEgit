e0a3e1e67b7bf7b7c30f2869b44778469dd786f9
mahathisomisetty <mahathisomisetty777@gmail.com>
2023-05-25 20:37:53 +0530

    added components

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -3,16 +3,28 @@ import {MatDialog} from '@angular/material/dialog';
 import { LoginComponent } from '../login/login.component';
 import {FormControl} from '@angular/forms';
 import {MatDrawerMode} from '@angular/material/sidenav';
+import { AdddoctorComponent } from './adddoctor/adddoctor.component';
+import { AddnurseComponent } from './addnurse/addnurse.component';
+
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
-  constructor(public dialog: MatDialog) {}
+  title ='angular-dialog';
+  constructor(public MatDialog: MatDialog) {}
 
   openDialog() {
-    this.dialog.open(LoginComponent);
+    
+
+    this.MatDialog.open(AdddoctorComponent,{
+
+    }),
+    this.MatDialog.open(AddnurseComponent,{
+
+    })
   }
+  
  
 }
