8a2d76d9c525f713febe967ffae360a2f5cac7c9
mahathisomisetty <mahathisomisetty777@gmail.com>
2023-05-26 14:28:18 +0530

    updated doctor

--- src/app/doctor/doctordashboard/doctordashboard.component.ts
+++ src/app/doctor/doctordashboard/doctordashboard.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  
+];
 @Component({
   selector: 'app-doctordashboard',
   templateUrl: './doctordashboard.component.html',
   styleUrls: ['./doctordashboard.component.css']
 })
 export class DoctordashboardComponent {
+  currentDate: Date;
 
+  constructor() {
+    this.currentDate = new Date();
+  }
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  
 }
